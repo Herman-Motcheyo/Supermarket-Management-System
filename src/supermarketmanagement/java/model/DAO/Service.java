ff0eac06d18863cbb72debf11b1b3475d52e0d11
Herman-Motcheyo <hermanmotcheyo@gmail.com>
2020-09-12 23:08:01 +0100

    ajout de la class service

---
+++ src/supermarketmanagement/java/model/DAO/Service.java
@@ -0,0 +1,37 @@
+package supermarketmanagement.java.model.DAO;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.SQLException;
+
+
+public class Service {
+	
+	Statement st = null;
+	ResultSet rs = null;
+	String sql ;
+	Connection  connect= ConnectDatabase.getInstance(); 
+	
+	public ResultSet loadcombobos(String name) throws SQLException{
+		
+		if(name.equalsIgnoreCase("storehouse")) {
+		    sql = " select nameStore from storehouse";
+				st = connect.createStatement();
+				rs= st.executeQuery(sql);
+				return rs;
+	
+		}else if (name.equalsIgnoreCase("category")) {
+		     sql = " select nameCat from categorie";
+				st = connect.createStatement();
+				rs= st.executeQuery(sql);
+				return rs;
+		}else {
+		    sql = " select namePro from provider";
+				st = connect.createStatement();
+				rs= st.executeQuery(sql);
+				return rs;
+		}
+	
+	}
+}
